5b5dee3cb2a31a8fa5a6ec9f30bc875e249e5190
carlos-el <c-enriquez@outlook.com>
2020-07-28 12:11:26 +0200

    Añade variables para describir lugares y objetos

--- adventure-game-skill/src/app/variables.js
+++ adventure-game-skill/src/app/variables.js
@@ -1,14 +1,103 @@
-exports.playerName = function(voxaEvent) {
-  return voxaEvent.model.player.playerName;
+const Map = require("./content/map.js");
+const ElementCollection = require("./content/elementCollection.js");
+
+const m = new Map()
+const ec = new ElementCollection()
+
+
+
+exports.playerName = function (voxaEvent) {
+  return voxaEvent.model.choices.name;
 };
 
-exports.playerSex = function(voxaEvent) {
-  return voxaEvent.model.player.playerSex;
+exports.playerSex = function (voxaEvent) {
+  return voxaEvent.model.choices.sex;
 };
 
-exports.playerDoorColor = function(voxaEvent) {
-  if (voxaEvent.model.player.playerSex === "male")
+exports.sexSuffix = function (voxaEvent) {
+  if (voxaEvent.model.choices.sex === "male")
+    return "o";
+  else if (voxaEvent.model.choices.sex === "female")
+    return "a";
+};
+
+exports.playerVoice = function (voxaEvent) {
+  if (voxaEvent.model.choices.sex === "male")
+    return "Miguel";
+  else if (voxaEvent.model.choices.sex === "female")
+    return "Lucia";
+};
+
+exports.playerDoorColor = function (voxaEvent) {
+  if (voxaEvent.model.choices.sex === "male")
     return "azul";
-  else if (voxaEvent.model.player.playerSex === "female")
+  else if (voxaEvent.model.choices.sex === "female")
     return "rosa";
 };
+
+exports.sandraVoice = function (voxaEvent) {
+  return "Conchita";
+};
+
+///// INSPECT VARIABLES /////
+exports.describeInspectElementOrObject = function (voxaEvent) {
+  if (voxaEvent.model.control.elementOrObjectToDescribeAlreadyInspected) {
+    return ec.elements[voxaEvent.model.control.elementOrObjectToDescribe].alreadyInspectedQuote;
+  } else {
+    return ec.elements[voxaEvent.model.control.elementOrObjectToDescribe].inspectQuote;
+  }
+};
+
+///// MAP EXPLORATION VARIABLES /////
+// Based on default map elements
+exports.describeFullQuote = function (voxaEvent) {
+  return describeLocation(voxaEvent) + " " + describeElements(voxaEvent) + " " + describeObjects(voxaEvent) + " " + describeNpcs(voxaEvent);
+};
+exports.describeFullQuoteWithStory = function (voxaEvent) {
+  return describeLocation(voxaEvent) + " " + describeStory(voxaEvent) + " " + describeElements(voxaEvent) + " " + describeObjects(voxaEvent) + " " + describeNpcs(voxaEvent);
+};
+
+
+
+
+
+describeLocation = function (voxaEvent) {
+  const intros = ["Parece que estoy en", "Estoy en", "Este lugar es", "Este sitio es", "Ahora me encuentro en"]
+  return intros[Math.floor(Math.random() * intros.length)] + " " + m.locations[voxaEvent.model.map.currentLocation].locationQuote;;
+};
+
+describeStory = function (voxaEvent) {
+  return m.locations[voxaEvent.model.map.currentLocation].storyQuote;
+};
+
+// Based on session model of the player and elements and objects Collections
+describeElements = function (voxaEvent) {
+  let res = "";
+  const intros = ["En la zona", "En el área", "En este lugar"]
+  const intros2 = ["puedo ver", "hay", "veo", "puedo ver que hay"]
+
+  // For each element in the elements object reflected in the session we get the mention quote
+  const length = Object.keys(voxaEvent.model.map.locations[voxaEvent.model.map.currentLocation].elements).length;
+  for (var i = 0; i < length; i++) {
+    res += ec.elements[Object.entries(voxaEvent.model.map.locations[voxaEvent.model.map.currentLocation].elements)[i][0]].mentionQuote;
+    // add conjuncion before last element
+    if (i === length - 2) {
+      res += " y ";
+    }
+    // add coma
+    if (i < length - 2) {
+      res += ", ";
+    };
+  }
+
+  return intros[Math.floor(Math.random() * intros.length)] + " " + intros2[Math.floor(Math.random() * intros2.length)] + " " + res + ".";
+};
+
+describeObjects = function (voxaEvent) {
+  return "Mencion objectos";
+};
+
+describeNpcs = function (voxaEvent) {
+  return "Mencion personajes";
+};
+
