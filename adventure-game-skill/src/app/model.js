72f55e205cd0c07f5fcc85d6fafe909c14f102cd
carlos-el <c-enriquez@outlook.com>
2020-07-28 12:15:14 +0200

    Añade varaibles de control y funciones para describir elementos y localizaciones, progresa #10 y #13

--- adventure-game-skill/src/app/model.js
+++ adventure-game-skill/src/app/model.js
@@ -1,17 +1,36 @@
 const _ = require("lodash");
+const Map = require("./content/map.js");
+const ElementCollection = require("./content/elementCollection.js");
+
+const m = new Map();
+const ec = new ElementCollection();
 
 class Model {
   constructor(data = {}) {
-    this.player = {playerName: null, 
-      playerSex: null //male or female
+    // Stores the players important choices in the game
+    this.choices = {};
+    // Stores the players discovered places and some infor about them.
+    this.map = {
+      obtained: false,
+      currentLocation: null,
+      mainLocations: [], //Name of the locations where the player can travel to
+      locations: {},
+    };
+    this.resources = {
+      junk: 0,
+      food: 0,
+      water: 0,
     };
     this.control = {
       confirmation: {
         nextState: null,
         previousState: null,
       },
-    }
-    
+      toFreeFromScene: false,
+      elementOrObjectToDescribe: "",
+      elementOrObjectToDescribeAlreadyInspected: false,
+    };
+
     _.assign(this, data);
   }
 
@@ -23,11 +42,22 @@ class Model {
     return this;
   }
 
-  // This function must be called in all states at the end 
-  setLastState(state) {
-    this.control.penultimateState = this.control.lastState;
-    this.control.lastState = state;
+  // Functions
+  // Gets element and its properties by name of the element
+  getElementProperties(elementName){
+    return ec.elements[elementName];
   }
+
+  // Adds a newly discovered location to the model
+  discoverLocation(location) {
+    // Add the location data to the model
+    this.map.locations[location] = m.getLocationDiscoverData(location, this.choices)
+    // If the location is main add it too
+    if(m.isMainLocation(location)){
+      this.map.mainLocations.push(location);
+    }
+  }
+
 }
 
 module.exports = Model;
