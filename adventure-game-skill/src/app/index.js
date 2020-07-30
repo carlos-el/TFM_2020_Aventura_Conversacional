3edcb9886f89d930a1bc6f3c98894b67cb9c6e53
carlos-el <c-enriquez@outlook.com>
2020-07-30 20:02:59 +0200

    Añade creación del DAO al empezar la app, progresa #8, #11 y #12

--- adventure-game-skill/src/app/index.js
+++ adventure-game-skill/src/app/index.js
@@ -5,6 +5,8 @@ const {
 } = require("voxa");
 const config = require("../config");
 const Model = require("./model");
+const UserDator = require("../services/userDator");
+const MongoClient = require('mongodb').MongoClient;
 const states = require("./states");
 const variables = require("./variables");
 const views = require("./views.json");
@@ -15,5 +17,21 @@ states(voxaApp);
 exports.alexaSkill = new AlexaPlatform(voxaApp);
 
 plugins.replaceIntent(voxaApp);
+
+/**
+ * Load User Dator into the model when the user starts the skill
+ */
+
+voxaApp.onRequestStarted(async (voxaEvent) => {
+  const userDator = await UserDator.getDator();
+  const game = await userDator.getUserGame(voxaEvent.user.id)
+
+  // If the player had a saved game then we load it
+  if (game) {
+    voxaEvent.model.game = game;
+  }
+
+  voxaEvent.user.userDator = userDator;
+});
 
 exports.voxaApp = voxaApp;
