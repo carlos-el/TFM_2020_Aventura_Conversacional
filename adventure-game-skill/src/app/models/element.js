0869ba66273a7e09f7917b65843ce1ae38c9146e
carlos-el <c-enriquez@outlook.com>
2020-08-07 17:38:33 +0200

    Actualiza con nueva propiedades para recibir el uso de un objeto, progresa #15

--- adventure-game-skill/src/app/models/element.js
+++ adventure-game-skill/src/app/models/element.js
@@ -5,6 +5,8 @@ module.exports = class Element {
         this.mentionAlreadyInspectedQuote = data.mentionAlreadyInspectedQuote // String, tells the description of the element if was already inspected
         this.inspectQuote = data.inspectQuote; // String, tells what is done when the element is inspected
         this.alreadyInspectedQuote = data.alreadyInspectedQuote; // String, tells what is done when the element is already inspected
+        this.useObjectQuote = data.useObjectQuote // String, tells what happens when an object is used in the lement successfully
         this.inspectActionTaken = data.inspectActionTaken; // Function for executing the effects of acting over the element
+        this.useObjectActionTaken = data.useObjectActionTaken; // Function for executing when an object is used over the element. Returns false if the action can not be performed
     }
 }
