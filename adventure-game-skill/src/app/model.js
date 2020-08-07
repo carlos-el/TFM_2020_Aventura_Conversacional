921c78d313c420d5f3daace02d33b06b113341c9
carlos-el <c-enriquez@outlook.com>
2020-08-07 10:53:44 +0200

    Añade inventario y funciones para obtener objetos del modelo por nombre, progresa #14

--- adventure-game-skill/src/app/model.js
+++ adventure-game-skill/src/app/model.js
@@ -1,21 +1,28 @@
 const _ = require("lodash");
 const Map = require("./models/map.js");
 const ElementCollection = require("./models/elementCollection.js");
+const ObjectCollection = require("./models/objectCollection.js");
 
 const m = new Map();
 const ec = new ElementCollection();
+const oc = new ObjectCollection();
 
 class Model {
   constructor(data = {}) {
     this.game = {
       // Stores the players important choices in the game
-      choices: {}, 
+      choices: {},
       // Stores the players discovered places and some infor about them.
       map: {
         obtained: false,
         currentLocation: null,
         mainLocations: [], //Name of the locations where the player can travel to
         locations: {},
+      },
+      inventory: {
+        obtained: true,
+        size: 3,
+        objects: {}
       },
       resources: {
         junk: 0,
@@ -31,7 +38,7 @@ class Model {
       },
       elementOrObjectToDescribe: "",
       elementOrObjectToDescribeAlreadyInspected: false,
-      pathToDescribe: {location: "", path: ""}
+      pathToDescribe: { location: "", path: "" }
     };
 
     _.assign(this, data);
@@ -45,10 +52,10 @@ class Model {
     return this;
   }
 
-  getDefultGameModel(){
+  getDefultGameModel() {
     return {
       // Stores the players important choices in the game
-      choices: {}, 
+      choices: {},
       // Stores the players discovered places and some infor about them.
       map: {
         obtained: false,
@@ -66,8 +73,49 @@ class Model {
 
   // Functions
   // Gets element and its properties by name of the element
-  getElementProperties(elementName){
+  getElementProperties(elementName) {
     return ec.elements[elementName];
+  }
+  getObjectProperties(objectName) {
+    return oc.objects[objectName];
+  }
+
+  // Gets elements or object identifier corresponding to the name passed as argument.
+  // The element or object must be in the currentLocation.
+  // Returns null if no object or element with that name is found in the currentLocation.
+  getCurrentLocationElementIdByName(elementName) {
+    for (let e in this.game.map.locations[this.game.map.currentLocation].elements) {
+      let ep = this.getElementProperties(e);
+      if (ep.names.includes(elementName)) {
+        return e;
+      }
+    }
+
+    return null;
+  }
+  getCurrentLocationObjectIdByName(objectName) {
+    for(let o in this.game.map.locations[this.game.map.currentLocation].objects){
+      let op = this.getObjectProperties(o);
+      if (op.names.includes(objectName)){
+        return o
+      }
+    }
+
+    return null;
+  }
+
+  // Gets object identifier corresponding to the name passed as argument.
+  // The object must be in the player inventory.
+  // Returns null if no object with that name is found in the inventory.
+  getInventoryObjectIdByName(objectName) {
+    for(let o in this.game.inventory.objects){
+      let op = this.getObjectProperties(o);
+      if (op.names.includes(objectName)){
+        return o
+      }
+    }
+
+    return null;
   }
 
   // Adds a newly discovered location to the model
@@ -75,7 +123,7 @@ class Model {
     // Add the location data to the model
     this.game.map.locations[location] = m.getLocationDiscoverData(location, this.game.choices)
     // If the location is main add it too
-    if(m.isMainLocation(location)){
+    if (m.isMainLocation(location)) {
       this.game.map.mainLocations.push(location);
     }
   }
