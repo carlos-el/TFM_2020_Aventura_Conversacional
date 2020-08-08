d0b8d633778bb7fbda5eee542e9e6dd600e56eaf
carlos-el <c-enriquez@outlook.com>
2020-08-08 10:59:39 +0200

    Añade nuevos objetos, closes #15

--- adventure-game-skill/src/app/models/objectCollection.js
+++ adventure-game-skill/src/app/models/objectCollection.js
@@ -7,7 +7,45 @@ module.exports = class objectCollection {
                 names: ["cizalla"],
                 isMale: false,
                 mentionQuote: "una cizalla",
-                inspectQuote: "Una cizalla. Puede servirme para cortar cables u objectos metálicos",
+                inspectQuote: "Una cizalla. Puede servirme para cortar cables u objectos metálicos.",
+                combineActionTaken: function (model, object) {
+                    return null;
+                },
+            }),
+            lanternEmpty: new Object({
+                names: ["linterna"],
+                isMale: false,
+                mentionQuote: "una linterna",
+                inspectQuote: "Una linterna sin bateria. Debería de encontrarle algunas antes de poder usarla.",
+                combineActionTaken: function (model, object) {
+                    if (object === "battery_small") {
+                        model.game.inventory.objects["lanternFull"] = 1;
+                        delete model.game.inventory.objects["lanternEmpty"];
+                        delete model.game.inventory.objects["battery_small"];
+
+                        return "lanternFull";
+                    }
+
+                    return null;
+                },
+            }),
+            lanternFull: new Object({
+                names: ["linterna"],
+                isMale: false,
+                mentionQuote: "una linterna",
+                inspectQuote: "Una linterna. Útil para iluminar algún sitio oscuro.",
+                combineActionTaken: function (model, object) {
+                    return null;
+                },
+            }),
+            battery_small: new Object({
+                names: ["pilas"],
+                isMale: false,
+                mentionQuote: "Una batería pequeña",
+                inspectQuote: "Una batería pequeña. Tal vez pueda ponersela a algún aparato",
+                combineActionTaken: function (model, object) {
+                    return null;
+                },
             }),
         }
     }
