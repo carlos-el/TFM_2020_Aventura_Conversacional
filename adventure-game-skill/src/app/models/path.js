fbf064b2f04a6e3cb2d4db6f8170a32c43dd9689
carlos-el <c-enriquez@outlook.com>
2020-08-08 17:58:36 +0200

    Cambia propiedad canGo a función, progresa #13

--- adventure-game-skill/src/app/models/path.js
+++ adventure-game-skill/src/app/models/path.js
@@ -4,7 +4,7 @@ module.exports = class Path {
         // An element can be used to trigger the lock or unlock of a road.
         // For locking the road the element action should set "canGo" to false and set the appropiate "problem" number in the model
         this.goesTo = data.goesTo; // String, identifier that tells where the path goes to
-        this.canGo = data.canGo; // Boolean, Tells if the player can go to this direction.
+        this.canGo = data.canGo; // Function, Tells if the player can go to this direction or the problem because it can not go
         this.problem = data.problem; // Number, If "canGo" is false, tells the number of the problem that is not letting the player go
         this.problemMentionQuotes = data.problemMentionQuotes; // Array of strings, each string tells the problem for crossing a path depending on the "problem" number
     }
