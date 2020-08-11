826aa5988edda5a25b62a09973066e6e6d423ec9
carlos-el <c-enriquez@outlook.com>
2020-08-11 13:02:15 +0200

    Añade nuevas localizaciones, npcs, y cambia parámetro de las funciones de acción, progresa #17

--- adventure-game-skill/src/app/models/map.js
+++ adventure-game-skill/src/app/models/map.js
@@ -7,18 +7,18 @@ module.exports = class Map {
         this.locations = {
             southForest1: new Location({
                 names: [], // (only needed if main == true)
-                locationQuote: "el bosque al sureste de la central hidroeléctrica.",
+                locationQuote: "el bosque al sureste de la central hidroeléctrica",
                 storyQuote: "Sandra dice que antes de la explosión a este sitio solía venir mucha gente a hacer picnics y deporte. Supongo que debe de haber perdido encanto desde entonces.",
                 main: false,
-                elements: function (choices) {
+                elements: function (game) {
                     return {
                         bag_1: false // False for and element means that the element has not being inspected by the player before
                     }
                 },
-                npcs: function (choices) {
+                npcs: function (game) {
                     return {};
                 },
-                objects: function (choices) {
+                objects: function (game) {
                     return {
                         metalShears: false // false for objects that were not dropped by the player
                     };
@@ -26,9 +26,10 @@ module.exports = class Map {
                 to: { // Contains the paths connected with this location
                     N: new Path({
                         goesTo: "hydroelectricPowerPlantOutskirts",
-                        canGo: function (model) {
+                        canGo: function (game) {
                             return true;
                         },
+                        go: function (game) {return "";},
                         problemMentionQuotes: [],
                     }),
                 }
@@ -38,53 +39,63 @@ module.exports = class Map {
                 locationQuote: "las afueras de la central hidroeléctrica.",
                 storyQuote: "No hay mucho que decir sobre este sitio.",
                 main: false,
-                elements: function (choices) {
+                elements: function (game) {
                     return {
                         door_metalFence_1_closed: false,
                     }
                 },
-                npcs: function (choices) {
+                npcs: function (game) {
                     return {};
                 },
-                objects: function (choices) {
+                objects: function (game) {
                     return {
                         lanternEmpty: false,
                     };
                 },
                 to: {
-                    S: new Path({
-                        goesTo: "southForest1",
-                        canGo: function (model) {
+                    N: new Path({
+                        goesTo: "road_Camp_hydroelectricPowerPlantOutskirts",
+                        canGo: function (game) {
                             return true;
                         },
+                        go: function (game) {return "";},
+                        problemMentionQuotes: [],
+                    }),
+                    S: new Path({
+                        goesTo: "southForest1",
+                        canGo: function (game) {
+                            return true;
+                        },
+                        go: function (game) {return "";},
                         problemMentionQuotes: [],
                     }),
                     O: new Path({
                         goesTo: "hydroelectricPowerPlant",
-                        canGo: function (model) {
-                            // If the door is close still
-                            if("door_metalFence_1_closed" in model.game.map.locations[model.game.map.currentLocation].elements){
+                        canGo: function (game) {
+                            // If the door is closed still
+                            if("door_metalFence_1_closed" in game.map.locations[game.map.currentLocation].elements){
                                 // return problem 0
                                 return 0;
                             }
                             return true;
                         },
+                        go: function (game) {return "";},
                         problemMentionQuotes: ["Hay una verja metálica con un candado que me impide el paso. Tal vez pueda encontrar algo con lo que desacerme del candado."],
                     }),
                 }
             }),
             hydroelectricPowerPlant: new Location({
                 names: ["la central", "central", "central hidroeléctrica", "lacentral hidroeléctrica"],
-                locationQuote: "la central hidroeléctrica.",
+                locationQuote: "la central hidroeléctrica",
                 storyQuote: "No hay mucho que decir sobre este sitio.",
                 main: true,
-                elements: function (choices) {
+                elements: function (game) {
                     return {}
                 },
-                npcs: function (choices) {
+                npcs: function (game) {
                     return {};
                 },
-                objects: function (choices) {
+                objects: function (game) {
                     return {
                         battery_small: false,
                     };
@@ -92,9 +103,78 @@ module.exports = class Map {
                 to: {
                     E: new Path({
                         goesTo: "hydroelectricPowerPlantOutskirts",
-                        canGo: function (model) {
+                        canGo: function (game) {
                             return true;
                         },
+                        go: function (game) {return "";},
+                        problemMentionQuotes: [],
+                    }),
+                }
+            }),
+            road_Camp_hydroelectricPowerPlantOutskirts: new Location({
+                names: [],
+                locationQuote: "el camino que comunica el campamento con la central hidroeléctrica",
+                storyQuote: "No hay mucho que decir sobre este sitio.",
+                main: false,
+                elements: function (game) {
+                    return {}
+                },
+                npcs: function (game) {
+                    // set npc initial state
+                    game.npcs["sandra"] = "1";
+                    return {
+                        sandra: false, // return npc with false (the player did not talk to him)
+                    };
+                },
+                objects: function (game) {
+                    return {};
+                },
+                to: {
+                    S: new Path({
+                        goesTo: "hydroelectricPowerPlantOutskirts",
+                        canGo: function (game) {
+                            return true;
+                        },
+                        go: function (game) {return "";},
+                        problemMentionQuotes: [],
+                    }),
+                    N: new Path({
+                        goesTo: "camp",
+                        canGo: function (game) {
+                            return true;
+                        },
+                        go: function (game) {return "";},
+                        problemMentionQuotes: [],
+                    }),
+                }
+            }),
+            camp: new Location({
+                names: ["el campamento", "campamento"],
+                locationQuote: "el campamento",
+                storyQuote: ". Este es el asentamiento al que vinimos a vivir unos cuantos supervivientes y yo poco despues de la explosión. Ojalá todavía fueramos tantos como antes.",
+                main: true,
+                elements: function (game) {
+                    return {}
+                },
+                npcs: function (game) {
+                    if (game.npcs["sandra"] == 2 ){
+                        return {
+                            sandra: false, // npc with false (the player did not talk to him)
+                        };
+                    }
+
+                    return {};
+                },
+                objects: function (game) {
+                    return {};
+                },
+                to: {
+                    S: new Path({
+                        goesTo: "road_Camp_hydroelectricPowerPlantOutskirts",
+                        canGo: function (game) {
+                            return true;
+                        },
+                        go: function (game) {return "";},
                         problemMentionQuotes: [],
                     }),
                 }
@@ -102,7 +182,7 @@ module.exports = class Map {
         }
     }
 
-    getLocationDiscoverData(location, choices) {
+    getLocationDiscoverData(location, game) {
         let paths = {};
         let key = "";
 
@@ -114,9 +194,9 @@ module.exports = class Map {
         }
 
         return {
-            elements: this.locations[location].elements(choices),
-            npcs: this.locations[location].npcs(choices),
-            objects: this.locations[location].objects(choices),
+            elements: this.locations[location].elements(game),
+            npcs: this.locations[location].npcs(game),
+            objects: this.locations[location].objects(game),
             to: paths,
         };
     }
