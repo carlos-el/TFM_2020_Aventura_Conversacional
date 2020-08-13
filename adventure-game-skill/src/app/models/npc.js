532275230605a7bf2e14f56a9cda947fb2379cb7
carlos-el <c-enriquez@outlook.com>
2020-08-13 23:56:04 +0200

    Añade propiedad mercader a los npcs, progresa #17

--- adventure-game-skill/src/app/models/npc.js
+++ adventure-game-skill/src/app/models/npc.js
@@ -1,7 +1,8 @@
 module.exports = class Npc {
     constructor(data) {
-        this.name = data.name; // String, name of the npc
+        this.names = data.names; // Array of strings, names of the npc
         this.voice = data.voice // String, voice of the actor
         this.states = data.states; // Object, states of the npc
+        this.merchant = data.merchant; //Object, defines the merchat qualities of the npc
     }
 }
