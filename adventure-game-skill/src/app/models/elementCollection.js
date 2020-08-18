da145a05cc3c317ff915ced893bce27a04ea305c
carlos-el <c-enriquez@outlook.com>
2020-08-18 17:50:00 +0200

    Cambios menores a los elementos

--- adventure-game-skill/src/app/models/elementCollection.js
+++ adventure-game-skill/src/app/models/elementCollection.js
@@ -7,12 +7,13 @@ module.exports = class ElementCollection {
                 names: ["bolsa", "bolsa de tela"],
                 mentionQuote: "una bolsa de tela que parece tener algo dentro. Debería inspeccionarla",
                 mentionAlreadyInspectedQuote: "Una bolsa de tela que ya he revisado",
-                inspectQuote: "Parece que dentro de la bolsa hay algo de chatarra. La cogeré, me vendrá bien.",
+                inspectQuote: "Parece que dentro de la bolsa hay algo de chatarra y agua. La cogeré, me vendrá bien.",
                 alreadyInspectedQuote: "Ya he revisado esta bolsa, no hay nada más de interés.",
                 useObjectQuote: "",
                 inspectActionTaken: function (game, alreadyInspected) {
                     if (!alreadyInspected){
                         game.resources.junk += 10;
+                        game.resources.water += 2;
                     }
                     return "";
                 },
