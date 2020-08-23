5182169cbdd521dc8d97eea9410d7ce1fcc3da23
carlos-el <c-enriquez@outlook.com>
2020-08-23 16:58:59 +0200

    Añade funciones para guardar trampas puestas en tiempo real

--- adventure-game-skill/src/services/userDator.js
+++ adventure-game-skill/src/services/userDator.js
@@ -6,7 +6,7 @@ module.exports = class UserDator {
         this.db = db;
     }
 
-    static async getDator(){
+    static async getDator() {
         // "config.mongodb.url" +"/"+ "config.mongodb.dbname"
         const database = await MongoClient.connect("mongodb://localhost:27017" + "/" + "adventure-game-skill");
         return new UserDator(database.db())
@@ -37,9 +37,9 @@ module.exports = class UserDator {
         // If the user is already in the database, update it.
         if (await this.db.collection('users').findOne({ userId: userId })) {
             console.log("Saving alternative game")
-            await this.db.collection('users').updateOne({ userId: userId }, { $set: { alternativeGame: {game: game, lastState: lastState} } })
+            await this.db.collection('users').updateOne({ userId: userId }, { $set: { alternativeGame: { game: game, lastState: lastState } } })
         } else { // If it is not then insert it.
-            await this.db.collection('users').insertOne({ userId: userId, game: game })
+            await this.db.collection('users').insertOne({ userId: userId, game: null, alternativeGame: { game: game, lastState: lastState } })
         }
     }
 
@@ -48,9 +48,8 @@ module.exports = class UserDator {
         if (await this.db.collection('users').findOne({ userId: userId })) {
             console.log("Removing alternative game")
             await this.db.collection('users').updateOne({ userId: userId }, { $set: { alternativeGame: null } })
-        } else { // If it is not then insert it.
-            await this.db.collection('users').insertOne({ userId: userId, game: game })
         }
+        // If it is not then do nothing.  
     }
 
     async getUserAlternativeGame(userId) {
@@ -63,6 +62,71 @@ module.exports = class UserDator {
             console.log("getUserAlternativeGame: null")
             return null;
         }
+    }
+
+    async saveUserTrapSet(userId, elementWithTrap, trapObjectUsed, elementToReplace, timeSet) {
+        const user = await this.db.collection("users").findOne({ userId: userId });
+        // If the user is in the database, update it.
+        if (user) {
+            console.log("Saving trap use")
+            let userTraps = user.traps;
+            // if the user has already traps add the trap and update the database
+            if (userTraps) {
+                userTraps[elementWithTrap] = { trapObjectUsed: trapObjectUsed, elementToReplace: elementToReplace, timeSet: timeSet };
+                await this.db.collection('users').updateOne({ userId: userId }, { $set: { traps: userTraps } })
+            } else {
+                //In other case just set the new trap
+                let traps = {};
+                traps[elementWithTrap] = { trapObjectUsed: trapObjectUsed, elementToReplace: elementToReplace, timeSet: timeSet };
+                await this.db.collection('users').updateOne({ userId: userId }, { $set: { traps: traps } })
+            }
+        } else { // If it is not then insert it.
+            console.log("Saving trap use without user being in the database")
+            // Just save the trap, the next time adding a trap the user will be already in the database
+            let traps = {};
+            traps[elementWithTrap] = { trapObjectUsed: trapObjectUsed, elementToReplace: elementToReplace, timeSet: timeSet };
+            await this.db.collection('users').insertOne({ userId: userId, game: null, traps: traps })
+        }
+    }
+
+    async getUserTrapSet(userId, elementWithTrap) {
+        const user = await this.db.collection("users").findOne({ userId: userId });
+        // If the user is  in the database
+        if (user) {
+            if(user.traps){
+                console.log("Getting trap: found")
+                return user.traps[elementWithTrap];
+            }
+
+            console.log("Getting trap: null")
+            return null;
+        } else { 
+            console.log("Getting trap: null")
+            return null;
+        }
+    }
+
+    async removeUserTrapSet(userId, elementWithTrap) {
+        const user = await this.db.collection("users").findOne({ userId: userId });
+        // If the user is already in the database, remove the trap.
+        if (user) {
+            let userTraps = user.traps;
+            // if the user has already traps add the trap and update the database
+            if (userTraps) {
+                delete userTraps[elementWithTrap];
+                await this.db.collection('users').updateOne({ userId: userId }, { $set: { traps: userTraps } })
+            }
+        } 
+        // in other case do nothing
+    }
+
+    async clearUserTrapSet(userId) {
+        const user = await this.db.collection("users").findOne({ userId: userId });
+        // If the user is already in the database, remove all traps.
+        if (user) {
+            await this.db.collection('users').updateOne({ userId: userId }, { $set: { traps: null } })
+        } 
+        // in other case do nothing
     }
 }
 
