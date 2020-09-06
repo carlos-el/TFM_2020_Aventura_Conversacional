fc56a2d384b0e0d71f2fc67d7b53dd0eeb44bd8c
carlos-el <c-enriquez@outlook.com>
2020-09-07 01:35:52 +0200

    Cambios menores a descripción.

--- adventure-game-skill/src/app/models/path.js
+++ adventure-game-skill/src/app/models/path.js
@@ -5,7 +5,7 @@ module.exports = class Path {
         // For locking the road the element action should set "canGo" to false and set the appropiate "problem" number in the model
         this.goesTo = data.goesTo; // String, identifier that tells where the path goes to
         this.canGo = data.canGo; // Function, Tells if the player can go to this direction or the problem because it can not go
-        this.go = data.go; // Function, execute the effects of going to the goesTo location, returns empty if the player should go to goes to or the name of the next scene
+        this.go = data.go; // Function, execute the effects of going to the goesTo location, returns empty if the player should go to goesTo or the name of the next scene
         this.problem = data.problem; // Number, If "canGo" is false, tells the number of the problem that is not letting the player go
         this.problemMentionQuotes = data.problemMentionQuotes; // Array of strings, each string tells the problem for crossing a path depending on the "problem" number
     }
