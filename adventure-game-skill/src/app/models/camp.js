b584c516b10dcf9f73dd75cf2ec865560f5c3783
carlos-el <c-enriquez@outlook.com>
2020-09-07 01:35:07 +0200

    Añadidas recompensas por nivel y función de nextLevel.

--- adventure-game-skill/src/app/models/camp.js
+++ adventure-game-skill/src/app/models/camp.js
@@ -3,13 +3,35 @@ const CampLevel = require("./campLevel.js");
 module.exports = class Camp {
     constructor(data) {
         this.relevantLevels = {
-            1: new CampLevel({
+            2: new CampLevel({
                 levelActionTaken: function (game) {
-                    console.log("executing function")
+                    game.map.locations[game.map.currentLocation].objects["hook"] = false;
+                    return "";
+                },
+                speech: "Por subir el campamento al nivel 2 puedo darte un objeto bastante útil, un gancho. Seguro que juntandolo con algo más le encuentras utilidad. Que no se te olvide cogerlo antes de irte.",
+            }),
+            3: new CampLevel({
+                levelActionTaken: function (game) {
                     game.inventory.size += 1;
                     return "";
                 },
-                speech: "Por subir el campamento al nivel uno te daré un módulo para que puedas llevar un objeto más en tu mochila.",
+                speech: "Por subir el campamento al nivel tres te daré un módulo para que puedas llevar un objeto más en tu mochila.",
+            }),
+            9: new CampLevel({
+                levelActionTaken: function (game) {
+                    game.map.locations[game.map.currentLocation].objects["trapVerySmall_1_01h"] = false;
+                    return "";
+                },
+                speech: "Por subir el campamento al nivel 9 te daré una botella de pesca. Si encuentras el lugar adecuado te servirá para atrapar peces muy fácilmente. Que no se te olvide cogerla antes de irte.",
+            }),
+            12: new CampLevel({
+                levelActionTaken: function (game) {
+                    game.resources.food += 60;
+                    game.resources.water += 60;
+                    game.resources.junk += 60;
+                    return "";
+                },
+                speech: "Vaya, es fantástico. Últimamente todo ha ido sobre ruedas y hemos conseguido manterne el campamento bastante bien. El resto de exploradores también han traido muchos recursos así que nos han sobrado bastantes. Gracias a todo eso, por subir el campamento al nivel 12 te daré todos los recursos que han sobrado.",
             }),
         }
     }
@@ -54,17 +76,17 @@ module.exports = class Camp {
         if (quantity < 41){ // 4 levels at cost 10
             return 10 - (quantity % 10)
         } else if (quantity < 101) { // 4 levels at cost 15
-            return 15 - ((40 - quantity) % 15)
+            return ((40 - quantity) % 15)
         } else if (quantity < 161) { // 3 levels at cost 20
-            return 20 - ((100 - quantity) % 20)
+            return ((100 - quantity) % 20)
         } else if (quantity < 215) { // 2 levels at cost 27
-            return 27 - ((160 - quantity) % 27)
+            return ((160 - quantity) % 27)
         } else if (quantity < 289) { // 2 levels at cost 37  // with 288 pack u have reached lvl 15
-            return 37 - ((214 - quantity) % 37)
+            return ((214 - quantity) % 37)
         } else if (quantity < 409) { // 2 levels at cost 60 
-            return 60 - ((288 - quantity) % 60)
+            return ((288 - quantity) % 60)
         } else { // the rest at cost 100
-            return 100 - ((408 - quantity) % 100)
+            return ((408 - quantity) % 100)
         }       
     }
 }
